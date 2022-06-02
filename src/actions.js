ddcc85e2567820ccfac035ec5e415bb7fa6c3486
Sanya <Sasha12121299@gmail.com>
2022-06-02 17:34:10 +0300

    bind creators

---
+++ src/actions.js
@@ -0,0 +1,6 @@
+
+
+export const inc = () => ({type: 'INC'});
+export const dec = () => ({type: 'DEC'});
+export const rnd = (value) => ({type: 'RND', payload: value});
+export const reset = () => ({type: 'RESET'});
--- src/index.js
+++ src/index.js
@@ -1,17 +1,60 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// import { createStore } from 'redux';
+import {legacy_createStore as createStore, bindActionCreators} from 'redux'
+import reducer from './reducer';
+// import {inc, dec, rnd, reset} from './actions'
+import * as actions from './actions'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const store = createStore(reducer);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const  {dispatch, subscribe, getState} = store;
+
+const update = () => {
+    document.getElementById('counter').textContent = getState().value;
+}
+
+// вызыв при изм state
+subscribe(update);
+// store.subscribe(() => {
+//     update()
+//     console.log(store.getState());
+// })
+
+// const bindActionCreator = (creator, dispatch) => (...args) => {
+//     dispatch(creator(...args));
+// }
+
+// const {incDispatch, decDispatch, rndDispatch} = bindActionCreators({
+//     incDispatch: inc,
+//     decDispatch: dec,
+//     rndDispatch: rnd
+// }, dispatch);
+const {inc, dec, rnd, reset} = bindActionCreators(actions, dispatch);
+
+document.getElementById('inc').addEventListener('click', inc);
+// document.getElementById('dec').addEventListener('click', decDispatch)
+document.getElementById('dec').addEventListener('click', dec)
+document.getElementById('rnd').addEventListener('click', () => {
+    const value = Math.floor(Math.random() * 10);
+    rnd(value);
+})
+document.getElementById('reset').addEventListener('click', () => {
+    dispatch(reset());
+})
+
+// let state = reducer(initialState, {type: 'INC'})
+// state = reducer(state, {type: 'INC'})
+// state = reducer(state, {type: 'INC'})
+// state = reducer(state, {type: 'INC'})
+// console.log(state);
+
+ReactDOM
+    .createRoot(document.getElementById('root'))
+    .render(
+        <React.StrictMode>
+            <>
+
+            </>
+        </React.StrictMode>
+    );
