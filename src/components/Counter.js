d12ca55930b6c18bd4cd83a95c22bd6633951432
Sanya <Sasha12121299@gmail.com>
2022-06-03 15:33:21 +0300

    194 connect

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,3 +1,22 @@
+// import { Component } from "react";
+import { connect } from "react-redux";
+import * as actions from '../actions'
+// import { bindActionCreators } from "redux";
+
+// class Counter extends Component {
+//     render() {
+//         const {counter, inc, dec, rnd, reset} = this.props;
+//         return (
+//             <div className="jumbotron">
+//                 <h1>{counter}</h1>
+//                 <button onClick={dec} className="btn btn-primary">DEC</button>
+//                 <button onClick={inc} className="btn btn-primary">INC</button>
+//                 <button onClick={rnd} className="btn btn-primary">RND</button>
+//                 <button onClick={reset} className="btn btn-primary">RESET</button>
+//             </div>
+//         )
+//     }
+// }
 
 const Counter = ({counter, inc, dec, rnd, reset}) => {
     return (
@@ -11,4 +30,14 @@ const Counter = ({counter, inc, dec, rnd, reset}) => {
     )
 }
 
-export default Counter;
+const mapStateToProps = (reduserState) => {
+    return {
+        counter: reduserState.value
+    }
+}
+
+// const mapDispatchToProps = (dispatch) => {
+//     return bindActionCreators(actions, dispatch)
+// }
+
+export default connect(mapStateToProps, actions)(Counter);
--- src/index.js
+++ src/index.js
@@ -1,74 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// import { createStore } from 'redux';
 import {legacy_createStore as createStore, bindActionCreators} from 'redux'
 import reducer from './reducer';
-// import {inc, dec, rnd, reset} from './actions'
-// import * as actions from './actions'
 import { Provider } from 'react-redux';
 
 import App from './components/App';
 
 const store = createStore(reducer);
 
-// const  {dispatch, subscribe, getState} = store;
-// const {inc, dec, rnd, reset} = bindActionCreators(actions, dispatch);
-// const update = () => {
-    
-// }
 ReactDOM
     .createRoot(document.getElementById('root'))
     .render(
         <React.StrictMode>
-            {/* <Counter 
-                counter={getState().value}
-                inc={inc}
-                dec={dec}
-                rnd={() => {
-                    const value = Math.floor(Math.random() * 10);
-                    rnd(value);
-                }}
-                reset={reset}
-            /> */}
             <Provider store={store}>
                 <App/>
             </Provider>
         </React.StrictMode>
     );
-// update();
-// вызыв при изм state
-// subscribe(update);
-// store.subscribe(() => {
-//     update()
-//     console.log(store.getState());
-// })
 
-// const bindActionCreator = (creator, dispatch) => (...args) => {
-//     dispatch(creator(...args));
-// }
-
-// const {incDispatch, decDispatch, rndDispatch} = bindActionCreators({
-//     incDispatch: inc,
-//     decDispatch: dec,
-//     rndDispatch: rnd
-// }, dispatch);
-
-
-// document.getElementById('inc').addEventListener('click', inc);
-// // document.getElementById('dec').addEventListener('click', decDispatch)
-// document.getElementById('dec').addEventListener('click', dec)
-// document.getElementById('rnd').addEventListener('click', () => {
-//     const value = Math.floor(Math.random() * 10);
-//     rnd(value);
-// })
-// document.getElementById('reset').addEventListener('click', () => {
-//     dispatch(reset());
-// })
-
-// let state = reducer(initialState, {type: 'INC'})
-// state = reducer(state, {type: 'INC'})
-// state = reducer(state, {type: 'INC'})
-// state = reducer(state, {type: 'INC'})
-// console.log(state);
-
-
+    
